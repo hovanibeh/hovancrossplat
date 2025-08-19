bbd344423bada0ef539065b2435f6232031c72a4
hovanibeh <127843816+hovanibeh@users.noreply.github.com>
2025-08-20 00:44:27 +0100

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace hovan_ibehcrossplat;
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,51 @@
+﻿using hovan_ibehcrossplat.Pages;
+
+using hovan_ibehcrossplat.ViewModels;
+
+using Microsoft.Maui.Controls;
+
+namespace hovan_ibehcrossplat;
+
+public static class MauiProgram
+
+{
+
+    public static MauiApp CreateMauiApp()
+
+    {
+
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+
+            .UseMauiApp<App>()
+
+            .ConfigureFonts(fonts =>
+
+            {
+
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+
+            });
+
+        // Register ViewModels
+
+        builder.Services.AddSingleton<GameViewModel>();
+
+        builder.Services.AddSingleton<HistoryViewModel>();
+
+        builder.Services.AddSingleton<SettingsViewModel>();
+
+        // Register Pages
+
+        builder.Services.AddSingleton<GamePage>();
+
+        builder.Services.AddSingleton<HistoryPage>();
+
+        builder.Services.AddSingleton<SettingsPage>();
+
+        return builder.Build();
+
+    }
+
+}
